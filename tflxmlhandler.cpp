fa921fb9f84e50d28cd6998f9efd9f6e8126a4c9
Mark Rankilor <mark.rankilor@gmail.com>
2012-07-10 00:40:05 +0100

    Flesh out TFLXmlHandler class with other Xml elements

--- tflxmlhandler.cpp
+++ tflxmlhandler.cpp
@@ -6,20 +6,47 @@ TFLXmlHandler::TFLXmlHandler() :
 {
 }
 
+bool TFLXmlHandler::startDocument()
+{
+    qDebug() << "Parsing started";
+
+    return true;
+}
+
+bool TFLXmlHandler::endDocument()
+{
+    qDebug() << "Parsing ended";
+
+    return true;
+}
+
 bool TFLXmlHandler::startElement(const QString &namespaceURI,
                                  const QString &localName,
                                  const QString &qName,
                                  const QXmlAttributes &atts)
 {
-    qDebug() << "Read start tag: " << localName << endl;
-    qDebug() << "Tag attributes: " << endl;
+    qDebug() << "---<"<< localName << ">-----------------------";
 
     for (int i = 0; i < atts.length(); i++)
     {
-        qDebug() << atts.localName(i) << " = " << atts.value(i) << endl;
+        qDebug() << "  " << atts.localName(i) << " = " << atts.value(i);
     }
 
-    qDebug() << "---------------------------------------" << endl;
+    return true;
+}
+
+bool TFLXmlHandler::characters(const QString &ch)
+{
+    qDebug() << ch;
+
+    return true;
+}
+
+bool TFLXmlHandler::endElement(const QString &namespaceURI,
+                               const QString &localName,
+                               const QString &qName)
+{
+    qDebug() << "---</"<< localName << ">----------------------";
 
     return true;
 }
--- tflxmlhandler.h
+++ tflxmlhandler.h
@@ -7,10 +7,21 @@ class TFLXmlHandler : public QXmlDefaultHandler
 {
 public:
     explicit TFLXmlHandler();
-    bool startElement(const QString &namespaceURI,
-                      const QString &localName,
-                      const QString &qName,
-                      const QXmlAttributes &atts);
+
+    virtual bool startDocument();
+
+    virtual bool endDocument();
+
+    virtual bool startElement(const QString &namespaceURI,
+                              const QString &localName,
+                              const QString &qName,
+                              const QXmlAttributes &atts);
+
+    virtual bool characters(const QString &ch);
+
+    virtual bool endElement(const QString &namespaceURI,
+                            const QString &localName,
+                            const QString &qName);
 };
 
 #endif // TFLXMLHANDLER_H
